e1af2eaa954a5ed160b38d06b2bcf034b8cde48b
giulliaVilanova <govilanova@gmail.com>
2024-11-25 20:57:02 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -26,8 +26,8 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-            // Refatorado para usar amountFor
-            double thisAmount = amountFor(each);
+            // Atualizado para usar getCharge
+            double thisAmount = each.getCharge();
 
             // Add frequent renter points
             frequentRenterPoints++;
@@ -44,32 +44,5 @@ public class Customer {
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
-    }
-
-    // Método extraído
-    private double amountFor(Rental aRental) {
-        double thisAmount = 0;
-
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (aRental.getDaysRented() > 2) {
-                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-
-            case Movie.NEW_RELEASE:
-                thisAmount += aRental.getDaysRented() * 3;
-                break;
-
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-
-        return thisAmount;
     }
 }
